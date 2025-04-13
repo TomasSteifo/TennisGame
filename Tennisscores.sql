9e294c894cdf13afe9337f942778d743edd74691
Tomas Steifo <166187079+TomasSteifo@users.noreply.github.com>
2025-04-13 14:05:50 +0200

    Add files via upload

---
+++ Tennisscores.sql
@@ -0,0 +1,8 @@
+SELECT TOP (1000) [Id]
+      ,[ServerName]
+      ,[ReceiverName]
+      ,[ServerScore]
+      ,[ReceiverScore]
+      ,[Winner]
+      ,[PlayedOn]
+  FROM [TennisScores].[dbo].[TennisScores]
