46bd066e3f3ef52ebdef6d989f284d454bb95fa7
TomasSteifo <Steifotomas@gmail.com>
2025-04-14 10:59:07 +0200

    Update// Tomas

---
+++ TennisGame/Data/TennisData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TennisGame.Models;
+
+namespace TennisGame.Data
+{
+    public static class TennisData
+    {
+        public static void SaveGameResult(
+            string connectionString,
+            string serverName,
+            string receiverName,
+            int serverScore,
+            int receiverScore,
+            string winner)
+        {
+            // Create a DbContext (if you’re not using dependency injection)
+            var optionsBuilder = new DbContextOptionsBuilder<TennisDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=PC\\SQLEXPRESS;Database=TennisScores;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;"
+            );
+            using (var context = new TennisDbContext(optionsBuilder.Options))
+            {
+                // Build the object to save
+                var result = new GameResult
+                {
+                    ServerName = serverName,
+                    ReceiverName = receiverName,
+                    ServerScore = serverScore,
+                    ReceiverScore = receiverScore,
+                    Winner = winner,
+                    PlayedOn = DateTime.Now
+                };
+
+                // Add and save
+                context.GameResults.Add(result);
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
---
+++ TennisGame/Data/TennisDbContext .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TennisGame.Models;
+
+namespace TennisGame.Data
+{
+    public class TennisDbContext : DbContext
+    {
+        public DbSet<GameResult> GameResults { get; set; }
+
+        public TennisDbContext(DbContextOptions<TennisDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GameResult>().ToTable("TennisScores");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
